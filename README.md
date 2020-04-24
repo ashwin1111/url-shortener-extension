0de7efaa08fd48ffb21204032aac6e2317bd70ba
ashwin1111 <winash1998@gmail.com>
2020-04-25 01:30:09 +0530

    extension - v1

--- README.md
+++ README.md
@@ -1,27 +1,3 @@
-# UrlShortenerExtension
+# [URL Shortener Extension](https://addons.mozilla.org/en-US/firefox/addon/urlll/)
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 8.3.20.
-
-## Development server
-
-Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
-
-## Code scaffolding
-
-Run `ng generate component component-name` to generate a new component. You can also use `ng generate directive|pipe|service|class|guard|interface|enum|module`.
-
-## Build
-
-Run `ng build` to build the project. The build artifacts will be stored in the `dist/` directory. Use the `--prod` flag for a production build.
-
-## Running unit tests
-
-Run `ng test` to execute the unit tests via [Karma](https://karma-runner.github.io).
-
-## Running end-to-end tests
-
-Run `ng e2e` to execute the end-to-end tests via [Protractor](http://www.protractortest.org/).
-
-## Further help
-
-To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI README](https://github.com/angular/angular-cli/blob/master/README.md).
+Simple URL shortener! URL shortener allows to reduce long links from Facebook, YouTube, Twitter, Linked In and top sites on the Internet, just paste the long URL and click the Shorten URL button. On the next screen, copy the shortened URL and share it on websites, chat and e-mail. Feel free to check the [Live Demo](https://addons.mozilla.org/en-US/firefox/addon/urlll/).
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,113 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'url-shortener-extension';
+
+export class AppComponent implements OnInit {
+  title: any;
+  data: any;
+  mymodel: any;
+  urlCollection = [];
+  name: any;
+  description: any;
+
+  constructor() { }
+
+  onCheckboxChange(url, title, event) {
+    if (event === true) {
+      this.urlCollection.push({
+        url: url,
+        title: title
+      });
+    } else if (event === false) {
+      for (let i = 0; i < this.urlCollection.length; i++) {
+        if (this.urlCollection[i].url === url) {
+          this.urlCollection.splice(i, 1);
+        }
+      }
+    }
+  }
+
+  export(name, title, description) {
+    chrome.storage.local.remove('token');
+    var apiData = {
+      urls: this.urlCollection,
+      collection_title: title,
+      name: name,
+      description: description,
+    };
+
+    chrome.tabs.query({ currentWindow: true }, async function (result) {
+      result.forEach(async function (tab) {
+        var i = 0;
+        if (tab.active === true) {
+          if (i === 0) {
+            chrome.tabs.executeScript(
+              tab.id, {
+              code: `
+              if (window.location.origin === 'https://app.urlll.xyz') {
+                chrome.storage.local.set({
+                  token: localStorage.getItem('x-access-token')
+              }, function () {
+              });
+              } else {
+                alert('please go to https://urlll.xyz and proceed with extension');
+                window.location.href = "https://urlll.xyz";
+              }
+                `},
+              function (results) {
+                chrome.storage.local.get('token', function (items) {
+                  var a = JSON.stringify(items);
+                  fetch('https://urlll.xyz/extension/bulk', {
+                    method: 'post',
+                    headers: {
+                      'Accept': 'application/json, text/plain, */*',
+                      'Content-Type': 'application/json',
+                      'x-access-token': JSON.parse(a).token
+                    },
+                    body: JSON.stringify(apiData)
+                  }).then(res => res.json())
+                    .then(res => alert(res.msg));
+                  chrome.storage.local.remove('token');
+                });
+              }
+            );
+          }
+          i++;
+        }
+      });
+    });
+  }
+
+  ngOnInit() {
+    var data = [{
+      title: "",
+      url: "dummy"
+    }];
+
+    chrome.bookmarks.getTree(process_bookmark);
+
+    setTimeout(() => {
+      this.data = data;
+    }, 200);
+
+    function process_bookmark(bookmarks) {
+      for (var i = 0; i < bookmarks.length; i++) {
+        var bookmark = bookmarks[i];
+        if (bookmark.url) {
+          data.push({
+            title: bookmark.title,
+            url: bookmark.url
+          });
+        }
+
+        if (bookmark.children) {
+          process_bookmark(bookmark.children);
+        }
+      }
+    }
+  }
 }
